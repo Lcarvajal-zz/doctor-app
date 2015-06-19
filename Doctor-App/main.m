1fddff02063b5e539c683d0da77808699ddf6965
Lukas Carvajal <lukascarvajal@gmail.com>
2015-06-19 19:22:43 -0400

    Initial Commit

---
+++ Doctor-App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Doctor-App
+//
+//  Created by Lukas Carvajal on 6/19/15.
+//  Copyright (c) 2015 Lukas Carvajal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
